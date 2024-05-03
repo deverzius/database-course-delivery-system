cb2a0fd1e6db09d62e01e7c5f8d95970e0d70bc9
deverzius <nguyenmanh.4949@gmail.com>
2024-05-03 22:01:38 +0700

    add create db and create table script

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin, messages
 from apps.account.models import Account
 
 # Register your models here.
-class AccountAdmin(admin.ModelAdmin):
-    ordering = ["id"]
-
-admin.site.register(Account, AccountAdmin)
+admin.site.register(Account)
--- apps/account/views.py
+++ apps/account/views.py
@@ -17,3 +17,4 @@ class AccountViewSet(viewsets.ModelViewSet):
             return Response({
                 "data": Account.objects.all()
             })
+        
